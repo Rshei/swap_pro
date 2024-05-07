6d7cc24f15a1d7fc4e85348c4e09a4040819b3a6
Rshei <118082280+Rshei@users.noreply.github.com>
2024-05-07 21:01:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pymongo
 # Uses st.cache_resource to only run once.
 @st.cache_resource
 def init_connection():
-    return pymongo.MongoClient(**st.secrets["swaping"])
+    return pymongo.MongoClient(**st.secrets["mongo"])
 
 client = init_connection()
 
