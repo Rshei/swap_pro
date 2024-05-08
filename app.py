0803c5c5fe7653585e689896256548e65dde68e1
Rshei <118082280+Rshei@users.noreply.github.com>
2024-05-08 20:47:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,7 @@ import pymongo
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://raananshein:SlWTRvwKEkoAgMvM@swaping.dwpd43n.mongodb.net/?retryWrites=true&w=majority&appName=swaping"
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
